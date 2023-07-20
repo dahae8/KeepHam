4c76d90856683ad9cc0e06019d64aebe6d8abb38
김현종 <dw3510@naver.com>
2023-07-20 10:05:50 +0900

    Feat : Filter를 통해 로그 기능 custom
    상세한 log를 보기 위해 filter를 통해 log 커스텀

--- BACKEND/src/main/java/com/ssafy/keepham/config/objectmapper/ObjectMapperConfig.java
+++ BACKEND/src/main/java/com/ssafy/keepham/config/objectmapper/ObjectMapperConfig.java
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.SerializationFeature;
-import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
