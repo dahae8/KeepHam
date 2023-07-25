230a4133dce60ddf878eb8c9e8b8af01962b088c
이성연 <chamsy39@gmail.com>
2023-07-25 14:15:01 +0900

    TEST:branch 변경 및 리액트 코드 병합

---
+++ practice/src/components/UpdateContent.js
@@ -0,0 +1,31 @@
+import React, { Component } from "react";
+
+class UpdateContents extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Update</h1>
+        <form
+          action="/create_process"
+          method="post"
+          onSubmit={function (e) {
+            e.preventDefault();
+            this.props.onSubmit(e.target.title.value, e.target.desc.value);
+          }.bind(this)}
+        >
+          <p>
+            <input type="text" name="title" placeholder="title"></input>
+          </p>
+          <p>
+            <textarea name="desc" placeholder="description"></textarea>
+          </p>
+          <p>
+            <input type="submit"></input>
+          </p>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default UpdateContents;
