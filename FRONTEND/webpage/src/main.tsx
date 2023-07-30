91e501b862b0151d77b78d9a7dc559f20db80cf1
dhkim6956 <dhkim6956@naver.com>
2023-07-31 08:20:19 +0900

    Design: Add styles

--- FRONTEND/webpage/src/main.tsx
+++ FRONTEND/webpage/src/main.tsx
@@ -9,7 +9,9 @@ import SignUp, { action as signUpAction } from "./Components/SignUp/signUp.tsx";
 
 // Styles
 import "./styles/global.ts";
+import { createTheme, ThemeProvider } from "@mui/material";
 
+// React Router
 const router = createBrowserRouter([
   {
     path: "/",
@@ -24,8 +26,31 @@ const router = createBrowserRouter([
   },
 ]);
 
+// Mui Theme
+declare module '@mui/material/styles' {
+  interface Theme {
+    status: {
+      danger: string;
+    };
+  }
+  interface ThemeOptions {
+    status?: {
+      danger?: string;
+    }
+  }
+}
+
+const theme = createTheme({
+  typography: {
+    fontFamily: 'Pretendard',
+    htmlFontSize: 10,
+  }
+})
+
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <ThemeProvider theme={theme}>
+      <RouterProvider router={router} />
+    </ThemeProvider>
   </React.StrictMode>
 );
