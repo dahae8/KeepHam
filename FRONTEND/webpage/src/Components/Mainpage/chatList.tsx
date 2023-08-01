25ac4fa2fa352341f32c16f425c28c8b105433db
이성연 <chamsy39@gmail.com>
2023-08-01 16:13:53 +0900

    FE : 위치검색,함리트스출력 완성
    채팅방 목록 띄우기 구현 해야함
    그전에 컴포넌트 정리를 위한 merge

--- FRONTEND/webpage/src/Components/Mainpage/chatList.tsx
+++ FRONTEND/webpage/src/Components/Mainpage/chatList.tsx
@@ -5,7 +5,7 @@ export default function ChatList() {
   return (
     <div>
       <Typography variant="h3" gutterBottom>
-        fsdfsf
+        채팅방 목록들 들어갈 자리
       </Typography>
     </div>
   );
