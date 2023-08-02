18b19ec4597547c87113ffef77b79ebb8fb2741b
김현종 <dw3510@naver.com>
2023-08-02 17:43:46 +0900

    Feat : mongoDB 문제로 임시로 채팅 저장 주석처리

--- BACKEND/src/main/java/com/ssafy/keepham/domain/chat/controller/ChatController.java
+++ BACKEND/src/main/java/com/ssafy/keepham/domain/chat/controller/ChatController.java
@@ -40,6 +40,7 @@ public class ChatController {
     @SendTo("/topic/group/{roomId}")
     public Message sendMessageToRoom(@Payload Message message, @DestinationVariable Long roomId){
         log.info("message : {}", message);
+//        messageRepository.save(message);
         return chatRoomManager.sendMessageToRoom(message, roomId);
     }
 
