0401cc086e9eb3b301f5fa802ae513d09f4cb3bd
소영섭 <9412sys@gmail.com>
2023-08-03 00:53:04 +0900

    fix: 요기요 크롤링

--- crwl/crwl2.py
+++ crwl/crwl2.py
@@ -1,5 +1,5 @@
 # 라이브러리 import 
-from bs4 import BeautifulSoup
+# from bs4 import BeautifulSoup
 import requests
 import time 
 import random 
@@ -60,7 +60,7 @@ for category_value in category_list:
                 info_list.append(restaurant.get(key))
         
         df_list.append(info_list)
-# print(df_list)
+
 # DataFrame으로 표출하기
 # df = pd.DataFrame(df_list, columns=["category"] + key_list)
 # print(df.head(10))
@@ -72,70 +72,39 @@ menu_url = 'https://www.yogiyo.co.kr/api/v1/restaurants/{restaurant_id}/menu/?ad
 scraper = cloudscraper.create_scraper()
 
 ## 메뉴 중 가장 큰 파트
-def create_submenu_table(cursor):
-    # Define the table schema for SubMenu
-    create_table_query = """
-        CREATE TABLE IF NOT EXISTS SubMenu (
-            id INTEGER AUTO_INCREMENT PRIMARY KEY,
-            multiple VARCHAR(255),
-            name VARCHAR(255),
-            multiple_count INT,
-            has_deposit BOOLEAN,
-            is_available_quantity BOOLEAN,
-            slug VARCHAR(255),
-            subchoices JSON,
-            mandatory BOOLEAN
-            )
-    """
-    cursor.execute(create_table_query)
-
-for i in range(1):
-    menu_list = []
-    for item in df_list:
-        restaurant_id = item[1]  # The "id" is the first element in the list
-        menu_url_formatted = menu_url.format(restaurant_id=restaurant_id)
-        res_menu = scraper.get(menu_url_formatted, headers=headers)
-        menu_data = json.loads(res_menu.content)
-        menu_list.append(menu_data)
-        menu_data = menu_list[0]  # menu_list에서 첫 번째 레스토랑의 메뉴 정보를 가져옵니다.
-        items = menu_data[0].get('items', [])
-        for item in items:
-            subchoices = item.get('subchoices', [])  # 각 메뉴 아이템의 'subchoices'를 가져옵니다.
-            ## 서브메뉴 파싱 파트
-            ## 서브메뉴의 각 key로 값을 추출한다.
-            subchoice_data_list = []
-            for subchoice in subchoices:
-                subchoice_data = {}
-                for key in key_list:
-                    subchoice_data[key] = subchoice.get(key)
-                    subchoice_data_list.append(subchoice_data)
-                    # query = insert_query = """
-            #         INSERT INTO SubMenu 
-            #         (`multiple`, `name`, `multiple_count`, `has_deposit`, `is_available_quantity`, `slug`, `mandatory`, `id`)
-            #         VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
-            #         """
-            # #         values = (
-            #             subchoice_data.get('multiple'),
-            #             subchoice_data.get('name'),
-            #             subchoice_data.get('multiple_count'),
-            #             subchoice_data.get('has_deposit'),
-            #             subchoice_data.get('is_available_quantity'),
-            #             subchoice_data.get('slug'),
-            #             subchoice_data.get('mandatory'),
-            #             subchoice_data.get('id')
-            # )
-                    # cursor.execute(query, values)
-              
-            subchoice_subchoice_data_list = []
-            subsubchoices = subchoice.get('subchoices', [])
-            for subsubchoice in subsubchoices:
-                # print(subsubchoice)
-                subsubchoice_data = {}
-                for key in key_list:
-                    subsubchoice_data[key] = subsubchoice.get(key)
-                subchoice_subchoice_data_list.append(subsubchoice_data)
-# print(subchoice_data_list)
+menu_list = []
+# for item in df_list:
+    # restaurant_id = item[1]  # The "id" is the first element in the list
+menu_url_formatted = menu_url.format(restaurant_id=577374)
+res_menu = scraper.get(menu_url_formatted, headers=headers)
+menu_data = json.loads(res_menu.content)
+menu_list.append(menu_data)
+menu_data = menu_list[0]  # menu_list에서 첫 번째 레스토랑의 메뉴 정보를 가져옵니다.
+items = menu_data[0].get('items', [])
 print(menu_list)
+    
+for item in items:
+    subchoices = item.get('subchoices', [])  # 각 메뉴 아이템의 'subchoices'를 가져옵니다.
+    ## 서브메뉴 파싱 파트
+    ## 서브메뉴의 각 key로 값을 추출한다.
+    subchoice_data_list = []
+    for subchoice in subchoices:
+        subchoice_data = {}
+        for key in key_list:
+            subchoice_data[key] = subchoice.get(key)
+            subchoice_data_list.append(subchoice_data)
+# print(subchoice_data.keys())
+# print(subchoice_data_list)
+    #     subchoice_subchoice_data_list = []
+    #     subsubchoices = subchoice.get('subchoices', [])
+    #     for subsubchoice in subsubchoices:
+    #         # print(subsubchoice)
+    #         subsubchoice_data = {}
+    #         for key in key_list:
+    #             subsubchoice_data[key] = subsubchoice.get(key)
+    #         subchoice_subchoice_data_list.append(subsubchoice_data)
+
+
 ## 기본 가게 데이터 파싱
 def save_to_mysql(data_list):
     try:
@@ -193,5 +162,58 @@ def save_to_mysql(data_list):
         # 연결을 닫습니다.
         connection.close()
 
-# save_to_mysql(df_list)
-# save_to_mysql2(df_list)
+def save_to_submenu(data_list):
+    try:
+        # MySQL 서버에 연결
+        connection = pymysql.connect(
+            host="localhost", 
+            user="root",
+            password="ssafy", 
+            database="pydb")
+        
+        with connection.cursor() as cursor:
+            # 테이블이 없다면 새로 생성
+            create_table_query = """
+            CREATE TABLE IF NOT EXISTS Store (
+                `index` BIGINT NOT NULL AUTO_INCREMENT,
+                `category` VARCHAR(255) NULL,
+                `store_id` BIGINT NULL,
+                `name` VARCHAR(255) NULL,
+                `address` VARCHAR(255) NULL,
+                `estimated_delivery_time` VARCHAR(255) NULL,
+                `min_order_amount` VARCHAR(255) NULL,
+                `delivery_fee_to_display`	VARCHAR(255)	NULL,
+                `lat` VARCHAR(255) NULL,
+                `lng` VARCHAR(255) NULL,
+                PRIMARY KEY (`index`)
+            )
+            """
+            cursor.execute(create_table_query)
+            
+            # 데이터를 테이블에 삽입
+            for data in data_list:
+                insert_query = """
+                INSERT INTO Store 
+                (`category`, `store_id`,`name`,`address`, `estimated_delivery_time`, `delivery_fee_to_display`, `min_order_amount`, `lat`, `lng`) 
+                VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)
+                """
+                cursor.execute(insert_query, (
+                    data[0],
+                    int(data[1]),
+                    data[2],
+                    data[3],
+                    data[4],
+                    data[5],
+                    data[6],
+                    float(data[7]),
+                    float(data[8])
+                ))
+            
+            # 변경사항을 커밋하여 저장
+            connection.commit()
+
+    except Exception as e:
+        print("Error:", e)
+    finally:
+        # 연결을 닫습니다.
+        connection.close()
