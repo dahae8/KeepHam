049b5e7669cc3f933be715204d3ad5b04bceb866
김현종 <dw3510@naver.com>
2023-08-03 17:12:44 +0900

    Feat : roomId를 통해 해당 방 채팅 로그를 불러오는 기능 작성

--- BACKEND/src/main/java/com/ssafy/keepham/domain/chat/service/MessageService.java
+++ BACKEND/src/main/java/com/ssafy/keepham/domain/chat/service/MessageService.java
@@ -1,8 +1,25 @@
 package com.ssafy.keepham.domain.chat.service;
 
+import com.ssafy.keepham.common.error.ErrorCode;
+import com.ssafy.keepham.common.exception.ApiException;
+import com.ssafy.keepham.domain.chat.db.Message;
+import com.ssafy.keepham.domain.chat.db.MessageRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
 @Service
+@RequiredArgsConstructor
 public class MessageService {
+
+    private final MessageRepository messageRepository;
+
+    public List<Message> findMessageLog(Long roomId){
+        var messageList = messageRepository.findAllByRoomIdOrderByTimestampDesc(roomId);
+        return Optional.ofNullable(messageList)
+                .orElseThrow(()->new ApiException(ErrorCode.BAD_REQUEST, "채팅 로그가 존재하지 않습니다."));
+    }
 
 }
