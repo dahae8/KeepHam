d36cfa517508ca80727ba52245f4caa8deff7d63
Strewbarry <chamsy39@gmail.com>
2023-08-03 21:50:35 +0900

    FE:build 정상화

--- FRONTEND/webpage/src/Components/Chatt/Chatt.tsx
+++ FRONTEND/webpage/src/Components/Chatt/Chatt.tsx
@@ -3,7 +3,6 @@ import { Typography } from "@mui/material";
 import AwsDataFetcher from "@/Components/Chatt/getDatatest";
 
 export default function ChattingBox() {
-  const [chatthistory, getChatt] = useState([]);
   const [chattForsend, getChattForsend] = useState("");
 
   const sendChatt = () => {
