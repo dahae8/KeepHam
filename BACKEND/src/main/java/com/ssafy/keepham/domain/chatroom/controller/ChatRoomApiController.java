f761177b1a1db1dc85bbdd4821e469f2cce8cce3
bellring <starringbell@gmail.com>
2023-08-03 23:58:21 +0900

    Feat : swagger에 설명 추가

--- BACKEND/src/main/java/com/ssafy/keepham/domain/chatroom/controller/ChatRoomApiController.java
+++ BACKEND/src/main/java/com/ssafy/keepham/domain/chatroom/controller/ChatRoomApiController.java
@@ -9,6 +9,7 @@ import com.ssafy.keepham.domain.chatroom.entity.enums.ChatRoomStatus;
 import com.ssafy.keepham.domain.chatroom.service.ChatRoomManager;
 import com.ssafy.keepham.domain.chatroom.service.ChatRoomService;
 import com.ssafy.keepham.security.TokenProvider;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -25,8 +26,8 @@ public class ChatRoomApiController {
     private final ChatRoomManager chatRoomManager;
     private final TokenProvider tokenProvider;
 
-
-    @PostMapping("/rooms/create")
+    @Operation(summary = "방생성")
+    @PostMapping("/rooms")
     private Api<ChatRoomResponse> createRoom(
             @RequestBody ChatRoomRequest chatRoomRequest,
             @RequestHeader("Authorization") String token
@@ -38,6 +39,7 @@ public class ChatRoomApiController {
         return Api.OK(res);
     }
 
+    @Operation(summary = "모든 채팅방 조회")
     @GetMapping("/rooms")
     private Api<List<ChatRoomResponse>> findAllOpenedRoom(
             @RequestParam ChatRoomStatus status,
@@ -47,6 +49,7 @@ public class ChatRoomApiController {
         return Api.OK(chatRoomService.openedRoom(status, page, pageSize));
     }
 
+    @Operation(summary = "boxId로 채팅방 조회")
     @GetMapping("/rooms/{boxId}")
     private Api<List<ChatRoomResponse>> findOpenedRoomByBoxId(
             @PathVariable Long boxId,
@@ -57,7 +60,7 @@ public class ChatRoomApiController {
         return Api.OK(chatRoomService.findOpenedRoomByBoxId(status, page, pageSize, boxId));
     }
 
-
+    @Operation(summary = "채팅방 입장")
     @GetMapping("/{roomId}")
     public Api<Object> enterRoom(@PathVariable Long roomId, @RequestHeader("Authorization") String token){
         String subject = tokenProvider.validateTokenAndGetSubject(token);
@@ -66,6 +69,7 @@ public class ChatRoomApiController {
         return Api.OK(nickName);
     }
 
+    @Operation(summary = "암호가 걸린 방 입장")
     @PostMapping("/{roomId}")
     public Api<Object> enterSecretRoom(@PathVariable Long roomId, @RequestBody RoomPassword password, @RequestHeader("Authorization") String token){
         String subject = tokenProvider.validateTokenAndGetSubject(token);
@@ -78,13 +82,14 @@ public class ChatRoomApiController {
         }
     }
 
-
+    @Operation(summary = "해당 채팅방에 현재 유저 전부 삭제")
     @GetMapping("/{roomId}/clear")
     public Api<String> clearRoom(@PathVariable Long roomId, @RequestHeader("Authorization") String token){
         chatRoomManager.allUserClear(roomId);
         return Api.OK("전체 삭제 성공");
     }
 
+    @Operation(summary = "해당 채팅방의 모든 유저 닉네임 조회")
     @GetMapping("/{roomId}/allUser")
     private Api<Set<String>> getAllUser(@PathVariable Long roomId, @RequestHeader("Authorization") String token){
         tokenProvider.validateTokenAndGetSubject(token);
