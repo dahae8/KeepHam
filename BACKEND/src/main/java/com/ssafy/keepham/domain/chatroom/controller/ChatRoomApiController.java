32d69b3bdde15db879ba8ad93526ced11c9561c1
bellring <starringbell@gmail.com>
2023-08-05 22:48:41 +0900

    FIx: 토큰 필터 적용 예정에 따른 개별 확인 제외
    현재 토큰 필터 작동이 정상적이지 않아 user정보를 받아올 수 없어 임시로 UUID를 유저 정보로 전달

--- BACKEND/src/main/java/com/ssafy/keepham/domain/chatroom/controller/ChatRoomApiController.java
+++ BACKEND/src/main/java/com/ssafy/keepham/domain/chatroom/controller/ChatRoomApiController.java
@@ -13,30 +13,33 @@ import com.ssafy.keepham.domain.chatroom.service.ChatRoomService;
 import com.ssafy.keepham.security.TokenProvider;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Set;
+import java.util.UUID;
 
 @RestController
 @RequiredArgsConstructor
 @CrossOrigin(originPatterns = "*")
 @RequestMapping("/api")
+@Slf4j
 public class ChatRoomApiController {
 
     private final ChatRoomService chatRoomService;
     private final ChatRoomManager chatRoomManager;
-    private final TokenProvider tokenProvider;
+    private Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+    String tempNickName = UUID.randomUUID().toString();
 
     @Operation(summary = "방생성")
     @PostMapping("/rooms")
     private Api<ChatRoomResponse> createRoom(
-            @RequestBody ChatRoomRequest chatRoomRequest,
-            @RequestHeader("Authorization") String token
+            @RequestBody ChatRoomRequest chatRoomRequest
     ){
-        var subject = tokenProvider.validateTokenAndGetSubject(token);
-        var userId =  tokenProvider.getUserFromSubject(subject).getUserId();
-        chatRoomRequest.setSuperUserId(userId);
+        chatRoomRequest.setSuperUserId(tempNickName);
         var res = chatRoomService.createRoom(chatRoomRequest);
         return Api.OK(res);
     }
@@ -59,29 +62,27 @@ public class ChatRoomApiController {
             @RequestParam(defaultValue = "1") int page,
             @RequestParam(defaultValue = "6") int pageSize
     ){
+
         return Api.OK(chatRoomService.findOpenedRoomByBoxId(status, page, pageSize, boxId));
     }
 
     @Operation(summary = "채팅방 입장")
     @GetMapping("/{roomId}")
-    public Api<Object> enterRoom(@PathVariable Long roomId, @RequestHeader("Authorization") String token){
-        String subject = tokenProvider.validateTokenAndGetSubject(token);
-        var nickName = tokenProvider.getUserFromSubject(subject).getNickName();
+    public Api<Object> enterRoom(@PathVariable Long roomId){
         if (chatRoomManager.isSecretRoom(roomId)){
            throw new ApiException(ChatRoomError.SECRET_ROOM);
         }
-        chatRoomManager.userJoin(roomId, nickName);
-        return Api.OK(nickName);
+
+        chatRoomManager.userJoin(roomId, tempNickName);
+        return Api.OK(tempNickName);
     }
 
-    @Operation(summary = "방 입장")
+    @Operation(summary = "비밀 방 입장")
     @PostMapping("/{roomId}")
-    public Api<Object> enterSecretRoom(@PathVariable Long roomId, @RequestBody RoomPassword password, @RequestHeader("Authorization") String token){
-        String subject = tokenProvider.validateTokenAndGetSubject(token);
-        var nickName = tokenProvider.getUserFromSubject(subject).getNickName();
+    public Api<Object> enterSecretRoom(@PathVariable Long roomId, @RequestBody RoomPassword password){
         if (chatRoomManager.isPasswordCorrect(roomId, password.getPassword())){
-            chatRoomManager.userJoin(roomId, nickName);
-            return Api.OK(nickName);
+            chatRoomManager.userJoin(roomId, tempNickName);
+            return Api.OK(tempNickName);
         } else {
             return Api.ERROR(ErrorCode.BAD_REQUEST, "방 비밀번호가 일치하지 않습니다.");
         }
@@ -89,15 +90,14 @@ public class ChatRoomApiController {
 
     @Operation(summary = "해당 채팅방에 현재 유저 전부 삭제")
     @GetMapping("/{roomId}/clear")
-    public Api<String> clearRoom(@PathVariable Long roomId, @RequestHeader("Authorization") String token){
+    public Api<String> clearRoom(@PathVariable Long roomId){
         chatRoomManager.allUserClear(roomId);
         return Api.OK("전체 삭제 성공");
     }
 
     @Operation(summary = "해당 채팅방의 모든 유저 닉네임 조회")
     @GetMapping("/{roomId}/allUser")
-    private Api<Set<String>> getAllUser(@PathVariable Long roomId, @RequestHeader("Authorization") String token){
-        tokenProvider.validateTokenAndGetSubject(token);
+    private Api<Set<String>> getAllUser(@PathVariable Long roomId){
         return Api.OK(chatRoomManager.getAllUser(roomId));
     }
 
