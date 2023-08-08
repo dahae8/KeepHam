3206105a9d81710d3eb41716d0ead1ee9fd893d9
dhkim6956 <dhkim6956@naver.com>
2023-08-08 14:48:53 +0900

    Add: missing packages

--- FRONTEND/webpage/src/Pages/ChatRoom/ChatRoom.tsx
+++ FRONTEND/webpage/src/Pages/ChatRoom/ChatRoom.tsx
@@ -200,6 +200,7 @@ function ChatRoom() {
                       width: "100%",
                       height: "100%",
                     }}
+                    // size="large"
                   >
                     <FoodBank />
                   </IconButton>
