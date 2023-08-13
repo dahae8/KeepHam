259b8aa895dd589d52a3eba3aa7f22cf8728f37e
dhkim6956 <dhkim6956@naver.com>
2023-08-14 02:34:44 +0900

    Feat: add QR code generator

--- FRONTEND/webpage/src/Components/ChatRoom/BoxSettings.tsx
+++ FRONTEND/webpage/src/Components/ChatRoom/BoxSettings.tsx
@@ -1,50 +1,95 @@
 import { useState } from "react";
-import { Box,TextField  } from "@mui/material";
-import Button from '@mui/material/Button';
-
+import { Box, Divider, Grid, TextField, Typography } from "@mui/material";
+import Button from "@mui/material/Button";
+import QRCode from "react-qr-code";
 
 type propsType = {
   getPassword: (e: number) => void;
-  allow:()=>void;
+  allow: () => void;
+  roomPw: number;
 };
 
 function BoxSettings(props: propsType) {
   const [pwValue, setPwValue] = useState<number>(0);
-
+  
   return (
     <>
       <Box
-        component="main"
         sx={{
-          flexGrow: 1,
-          p: 3,
-          width: 3,
-          padding: 3,
-        }}
-      ></Box>
-      <TextField
-          id="standard-password-input"
-          label="Password"
-          autoComplete="current-password"
-          variant="filled"
-          onChange={(e) => {
-            setPwValue(Number(e.target.value));
-          }}
-        />
-      <Button variant="contained"
-        onClick={() => {
-          if (pwValue !== null) props.getPassword(pwValue);
+          margin: 2,
         }}
       >
-        비밀번호 설정
-      </Button>
-      <Button variant="contained"
-        onClick={() => {
-          props.allow();
-        }}
-      >
-        메뉴선택잠그기
-      </Button>
+        <Grid container spacing={2}>
+          <Grid item xs={12}>
+            <Typography variant="h6" color={"white"}>
+              함 정보
+            </Typography>
+            <Divider />
+          </Grid>
+          <Grid item xs={12}>
+            <Typography variant="h6" color={"white"}>
+              함 비밀번호
+            </Typography>
+            <Divider />
+          </Grid>
+          <Grid item xs={8}>
+            <TextField
+              label="Password"
+              autoComplete="current-password"
+              variant="standard"
+              onChange={(e) => {
+                setPwValue(Number(e.target.value));
+              }}
+            />
+          </Grid>
+          <Grid item xs={4}>
+            <Button
+              variant="contained"
+              onClick={() => {
+                if (pwValue !== null) props.getPassword(pwValue);
+              }}
+            >
+              설정
+            </Button>
+          </Grid>
+          <Grid item xs={12}>
+            <Button
+              variant="contained"
+              onClick={() => {
+                props.allow();
+              }}
+            >
+              메뉴선택잠그기
+            </Button>
+          </Grid>
+          <Grid item xs={12}>
+            <Box
+              sx={{
+                width: "100%",
+                height: "100%",
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
+              <Box
+                sx={{
+                  width: 216,
+                  height: 216,
+                  padding: 1,
+                  backgroundColor: "white",
+                  borderRadius: 1,
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                }}
+              >
+                <QRCode value={props.roomPw.toString()} />
+              </Box>
+            </Box>
+          </Grid>
+        </Grid>
+      </Box>
     </>
   );
 }
