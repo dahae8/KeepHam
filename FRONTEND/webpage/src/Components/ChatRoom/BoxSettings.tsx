bd6beea6d2d6f9705e64c25c92768b44b1a9254d
Strewbarry <chamsy39@gmail.com>
2023-08-13 22:05:55 +0900

    FE: 메뉴 선택화면 구현
    메뉴선택창 구현완료
    버튼 동작완료
    각 메뉴별 배열, count값 추가 필요 >> 현재는 닫으면 초기화됨
    메뉴선택 잠그기 구성 완료 >> 현재 상태 표기 필요

--- FRONTEND/webpage/src/Components/ChatRoom/BoxSettings.tsx
+++ FRONTEND/webpage/src/Components/ChatRoom/BoxSettings.tsx
@@ -1,8 +1,11 @@
 import { useState } from "react";
-import { Box } from "@mui/material";
+import { Box,TextField  } from "@mui/material";
+import Button from '@mui/material/Button';
+
 
 type propsType = {
   getPassword: (e: number) => void;
+  allow:()=>void;
 };
 
 function BoxSettings(props: propsType) {
@@ -19,19 +22,29 @@ function BoxSettings(props: propsType) {
           padding: 3,
         }}
       ></Box>
-      <input
-        type="text"
-        onChange={(e) => {
-          setPwValue(Number(e.target.value));
-        }}
-      ></input>
-      <button
+      <TextField
+          id="standard-password-input"
+          label="Password"
+          autoComplete="current-password"
+          variant="filled"
+          onChange={(e) => {
+            setPwValue(Number(e.target.value));
+          }}
+        />
+      <Button variant="contained"
         onClick={() => {
           if (pwValue !== null) props.getPassword(pwValue);
         }}
       >
         비밀번호 설정
-      </button>
+      </Button>
+      <Button variant="contained"
+        onClick={() => {
+          props.allow();
+        }}
+      >
+        메뉴선택잠그기
+      </Button>
     </>
   );
 }
--- FRONTEND/webpage/src/Components/ChatRoom/SelectItems.tsx
+++ FRONTEND/webpage/src/Components/ChatRoom/SelectItems.tsx
@@ -1,8 +1,59 @@
-function SelectItems() {
+import * as React from "react";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import Divider from "@mui/material/Divider";
+import ListItemText from "@mui/material/ListItemText";
+import ListItemAvatar from "@mui/material/ListItemAvatar";
+import Avatar from "@mui/material/Avatar";
+import Typography from "@mui/material/Typography";
+import Button from '@mui/material/Button';
+
+type propsType = {
+  name: string;
+  price: number;
+  image: string;
+  allow:boolean;
+  key:number;
+};
+
+function SelectItems(props: propsType) {
+  const [count, setCount] = React.useState(0);
   return (
-    <>
-      <div>SelectItems</div>
-    </>
+    <List sx={{ width: "100%", maxWidth: 360, bgcolor: "background.paper" }}>
+      <ListItem alignItems="flex-start" key={props.key}>
+        <ListItemAvatar>
+          <Avatar alt="Remy Sharp" src={props.image} />
+        </ListItemAvatar>
+        <ListItemText
+          primary={props.name}
+          secondary={
+            <React.Fragment>
+              {props.price}
+              {"원"}
+            </React.Fragment>
+          }
+        />
+        <Button onClick={()=>{
+          if(props.allow){  
+            setCount(count+1);
+          }
+        }}>+</Button>
+        <Typography
+          sx={{ display: "inline" }}
+          component="span"
+          variant="h6"
+          color="text.primary"
+        >
+          {count}
+        </Typography>
+        <Button onClick={()=>{
+          if(count>0&&props.allow){
+            setCount(count-1);
+          }
+        }}>-</Button>
+      </ListItem>
+      <Divider variant="inset" component="li" />
+    </List>
   );
 }
 
