55718d67c74cbe6b07bd537f5ee02d3abd34480b
dhkim6956 <dhkim6956@naver.com>
2023-08-16 10:32:28 +0900

    Refactor: Userlist refactoring

--- FRONTEND/webpage/src/Components/ChatRoom/User/Item.tsx
+++ FRONTEND/webpage/src/Components/ChatRoom/User/Item.tsx
@@ -1,33 +1,33 @@
-import * as React from 'react';
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import ListItemText from '@mui/material/ListItemText';
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
-import Divider from '@mui/material/Divider';
-import axios from 'axios';
+import { useEffect, useState } from "react";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemText from "@mui/material/ListItemText";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import Divider from "@mui/material/Divider";
+import axios from "axios";
 
-
-interface ItemProps {
+type ItemProps = {
   id: number;
   name: string;
-  roomId:number;
-  userNick:string|null;
-  superNick:string|null;
-}
+  roomId: number;
+  userNick: string | null;
+  superNick: string | null;
+  reloadUsers: () => void;
+};
 
-const Item: React.FC<ItemProps> = (props) => {
+function Item(props: ItemProps) {
   // let itemStyleByAmountType = "item " + props.amountType;
   // let fontStyleByAmountType = "fs-emphasis fc-red";
 
-  console.log("item에서 찍히는 userNick:", props.userNick)
-  console.log("item에서 찍히는 superNick:", props.superNick)
+  // console.log("item에서 찍히는 userNick:", props.userNick);
+  // console.log("item에서 찍히는 superNick:", props.superNick);
 
   const roomId = props.roomId;
   const itemTitle = props.name;
   const userNick = props.userNick;
   const superNick = props.superNick;
-  const AccessToken = sessionStorage.getItem("AccessToken")
+  const AccessToken = sessionStorage.getItem("AccessToken");
   // let itemAmount = "-" + addComma(props.amount.toString());
 
   // if (props.amountType === "충전") {
@@ -35,80 +35,110 @@ const Item: React.FC<ItemProps> = (props) => {
   //   itemAmount = itemAmount.replace("-", "+");
   // }
 
-  const exitUser = async () => {
-    console.log("Access Token",AccessToken)
-    try {
-      const url = import.meta.env.VITE_URL_ADDRESS + "/api/rooms/kick";
-      const response = await axios.put(url,
-        {
-          "kicked_user_nick_name": itemTitle,
-          "room_id": roomId
-        },
-        {
-          headers:
+  const [exitUserSwitch, setExitUserSwitch] = useState(false);
+  const [transSuperSwitch, setTransSuperSwitch] = useState(false);
+
+  useEffect(() => {
+    const exitUser = async () => {
+      console.log("Access Token", AccessToken);
+      try {
+        const url = import.meta.env.VITE_URL_ADDRESS + "/api/rooms/kick";
+        const response = await axios.put(
+          url,
           {
-            Authorization: `Bearer ` + AccessToken
-          }
-        })
-      console.log(response.data.body);
-
-      console.log(response);
-
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-  const transSuper = async () => {
-    try {
-      const url = import.meta.env.VITE_URL_ADDRESS + "/api/rooms/superUser";
-      const response = await axios.put(url,
-        {
-          "room_id": roomId,
-          "new_super_user": itemTitle
-        },
-        {
-          headers:
+            kicked_user_nick_name: itemTitle,
+            room_id: roomId,
+          },
           {
-            Authorization: `Bearer ` + AccessToken
+            headers: {
+              Authorization: `Bearer ` + AccessToken,
+            },
           }
-        })
-      console.log(response.data.body);
+        );
+        console.log(response.data.body);
 
-      console.log(response);
+        console.log(response);
+      } catch (error) {
+        console.log(error);
+      }
+    };
 
-    } catch (error) {
-      console.log(error);
+    if (exitUserSwitch) {
+      console.log("test");
+      exitUser();
+      props.reloadUsers();
+      setExitUserSwitch(true);
     }
-  };
+  }, [exitUserSwitch]);
 
+  useEffect(() => {
+    const transSuper = async () => {
+      try {
+        const url = import.meta.env.VITE_URL_ADDRESS + "/api/rooms/superUser";
+        const response = await axios.put(
+          url,
+          {
+            room_id: roomId,
+            new_super_user: itemTitle,
+          },
+          {
+            headers: {
+              Authorization: `Bearer ` + AccessToken,
+            },
+          }
+        );
+        console.log(response.data.body);
 
+        console.log(response);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    if (transSuperSwitch) {
+      console.log("test");
+      transSuper();
+      props.reloadUsers();
+      setTransSuperSwitch(false);
+    }
+  }, [transSuperSwitch]);
 
   return (
     <div>
-            <List>
-              {userNick === superNick && itemTitle !== superNick ? 
-                <ListItem
-                  secondaryAction={
-                    <div>
-                      <Button variant="contained" onClick={()=> {exitUser()}}><Typography>추방</Typography></Button>
-                      <Button variant="outlined" onClick ={()=> {transSuper()}}><Typography>위임</Typography></Button>
-                    </div> 
-                  }
+      <List>
+        {userNick === superNick && itemTitle !== superNick ? (
+          <ListItem
+            secondaryAction={
+              <div>
+                <Button
+                  variant="contained"
+                  onClick={() => {
+                    setExitUserSwitch(true);
+                  }}
                 >
-                  <ListItemText
-                    primary={itemTitle}
-                  />
-                  </ListItem>
-                :
-                <ListItem>
-                  <ListItemText primary={itemTitle} />
-                </ListItem>                
-              }
-              <Divider /> 
-            </List>
+                  <Typography>추방</Typography>
+                </Button>
+                <Button
+                  variant="outlined"
+                  onClick={() => {
+                    setTransSuperSwitch(true);
+                  }}
+                >
+                  <Typography>위임</Typography>
+                </Button>
+              </div>
+            }
+          >
+            <ListItemText primary={itemTitle} />
+          </ListItem>
+        ) : (
+          <ListItem>
+            <ListItemText primary={itemTitle} />
+          </ListItem>
+        )}
+        <Divider />
+      </List>
     </div>
   );
-};
+}
 
 export default Item;
--- FRONTEND/webpage/src/Components/ChatRoom/UserList.tsx
+++ FRONTEND/webpage/src/Components/ChatRoom/UserList.tsx
@@ -1,8 +1,7 @@
-import { Box } from "@mui/material"
-import Item from "./User/Item"
+import { Box } from "@mui/material";
+import Item from "@/Components/ChatRoom/User/Item.tsx";
 import axios from "axios";
 import { useEffect, useState } from "react";
-
 
 interface MyComponentProps {
   roomId: number;
@@ -11,80 +10,97 @@ interface MyComponentProps {
 }
 
 const UserList: React.FC<MyComponentProps> = (props) => {
-  console.log("방번호",props.roomId)
+  console.log("방번호", props.roomId);
   // const [userNick, setUserNick] = useState(sessionStorage.getItem("userNick"));
-  const [superNick, setSuperNick] = useState(sessionStorage.getItem("superUser"));
+  const [superNick, setSuperNick] = useState(
+    sessionStorage.getItem("superUser")
+  );
   const userNick = sessionStorage.getItem("userNick");
   // const superNick = sessionStorage.getItem("superUser");
 
   // console.log('userNick:',userNick)
   // console.log('superNick:',superNick)
 
-  const [users,setUsers] = useState([])
+  const [users, setUsers] = useState([]);
 
   // const users = ['사용자1', '사용자2', '사용자3']
 
-  useEffect(() => {
+  const [reload, setReload] = useState(false);
 
+  const reloadUsers = () => {
+    console.log("change reload");
+    console.log(reload);
+
+    setReload(!reload);
+    console.log(reload);
+  };
+
+  useEffect(() => {
     const fetchUser = async () => {
       try {
-        const url = import.meta.env.VITE_URL_ADDRESS + "/api/rooms/"+props.roomId+"/users";
+        const url =
+          import.meta.env.VITE_URL_ADDRESS +
+          "/api/rooms/" +
+          props.roomId +
+          "/users";
         const response = await axios.get(url);
         // console.log("결과:", response.data.body);
-  
+
         // console.log("결과2:",response);
         setUsers(response.data.body);
         // sessionStorage.setItem("superUser", )
-  
       } catch (error) {
         console.log(error);
       }
     };
     fetchUser();
-    console.log('users', users)
-  
-  }, []);
+    console.log("users", users);
+  }, [reload]);
 
   useEffect(() => {
-
     const fetchSuperUser = async () => {
       try {
-        const url = import.meta.env.VITE_URL_ADDRESS + "/api/rooms/"+props.boxId+"?status=OPEN&page=1&pageSize=1";
+        const url =
+          import.meta.env.VITE_URL_ADDRESS +
+          "/api/rooms/" +
+          props.boxId +
+          "?status=OPEN&page=1&pageSize=1";
         const response = await axios.get(url);
         // console.log("결과:", response.data.body[0].super_user_id);
-  
+
         // console.log("결과2:",response);
         setSuperNick(response.data.body[0].super_user_id);
         // sessionStorage.setItem("superUser", )
-  
       } catch (error) {
         console.log(error);
       }
     };
     fetchSuperUser();
-    console.log('SuperNick', superNick)
-  
-  }, [superNick]);
+    console.log("SuperNick", superNick);
+  }, [reload]);
 
   return (
-    <Box sx={{
-      padding: 2,
-      boxShadow: 4,
-      width: "100%",
-      height: "100%",
-    }}>
+    <Box
+      sx={{
+        padding: 2,
+        boxShadow: 4,
+        width: "100%",
+        height: "100%",
+      }}
+    >
       {users.map((item, index) => (
         <Item
           key={index}
           id={index}
           name={item}
           roomId={props.roomId}
-          userNick = {userNick}
-          superNick = {superNick}
+          userNick={userNick}
+          superNick={superNick}
+          reloadUsers={reloadUsers}
         />
-      ))}  
+      ))}
     </Box>
-  )
-}
+  );
+};
 
-export default UserList
+export default UserList;
